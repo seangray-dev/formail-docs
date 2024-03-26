c42d573e3bb5327cbb9846d69b4b9476d35f2743
Sean Gray <seangray.dev@gmail.com>
2024-03-26 17:26:50 -0400

    Update project configuration and license information

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig } from 'nextra-theme-docs';
+import React from 'react';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  banner: {
+    key: '1.0-release',
+    text: (
+      <a href='https://formail.dev' target='_blank'>
+        🎉 Formail 1.0 is released. Read more →
+      </a>
+    ),
+  },
+  logo: <span>Formail</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/seangray-dev/formail-app',
   },
-  chat: {
-    link: 'https://discord.com',
+  docsRepositoryBase: 'https://github.com/seangray-dev/formail-docs',
+  darkMode: false,
+  primaryHue: 0,
+  primarySaturation: 0,
+  toc: {
+    component: null,
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  navigation: false,
   footer: {
-    text: 'Nextra Docs Template',
+    component: null,
   },
-}
+};
 
-export default config
+export default config;
